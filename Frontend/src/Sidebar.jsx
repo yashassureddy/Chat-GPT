ff3589368fb66c8ad540964fc4a3f4307309de6a
yashassureddy <yashassureddykarnati@gmail.com>
2025-09-17 11:08:50 +0530

    Update Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -67,7 +67,7 @@ function Sidebar() {
     return (
         <section className="sidebar">
             <button onClick={createNewChat}>
-                <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+                <img src="/blacklogo.png" alt="gpt logo" className="logo"></img>
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
